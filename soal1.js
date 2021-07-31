7b3b5c150311d032c1641ec0951daedaabf4b098
abyanrf <rabyan78@gmail.com>
2021-07-31 09:57:49 +0700

    tugas evaluasi

---
+++ soal1.js
@@ -0,0 +1,5 @@
+var nilaiSantri = [ 52, 95, 55, 67, 79, 44, 30, 97, 84 ]
+    jumlah = 0;
+ for(i =0;i < nilaiSantri.length;i++){
+        jumlah += nilaiSantri[i]
+ }console.log(jumlah / nilaiSantri.length)
---
+++ soal3.js
@@ -0,0 +1,8 @@
+var panjang =prompt('masukan panjang');
+var lebar = prompt('masuk lebar');
+
+function luasPersegiPanjang(panjang,lebar){
+    return panjang * lebar
+}
+hasil = luasPersegiPanjang(panjang,lebar)
+document.write(hasil) 

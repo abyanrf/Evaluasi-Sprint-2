57cf243652d310a43e28ffbcdc57fb3b8b8aa5e5
abyanrf <rabyan78@gmail.com>
2021-07-31 10:55:58 +0700

    eavaluasi2

---
+++ soal5.js
@@ -0,0 +1,14 @@
+function  arrayToObject(firstName, lastName,gender,age){
+    orang :
+    arrayToObjec.namaDepan='firstName',
+    arrayToObjec.namaBelakang='lastName',
+    arrayToObjec.jenisKelamin='gender',
+    arrayToObjec.umur='age'
+};
+
+var people =[
+    ['abduh','latif','male',1992],
+    ['ahmad','taufiq','male',1985]
+];
+
+console.log(arrayToObject(people))
